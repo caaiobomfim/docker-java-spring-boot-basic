3b79a46dbd2811ef6c8ef915c4fddaaa976f75ea
Caio Bomfim <caaiobomfim@gmail.com>
2025-02-04 22:38:17 -0300

    Initial version of the Java Spring Boot project

---
+++ src/main/java/caaiobomfim/docker-java-spring-boot-basic/DockerJavaSpringBootBasicApplication.java
@@ -0,0 +1,13 @@
+package caaiobomfim.docker_java_spring_boot_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJavaSpringBootBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJavaSpringBootBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/caaiobomfim/docker_java_spring_boot_basic/DockerJavaSpringBootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package caaiobomfim.docker_java_spring_boot_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJavaSpringBootBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
